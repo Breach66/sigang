7cb86d682765231d16036a65d34647a3e6d410da
jjj <1@1.com>
2024-06-23 05:06:00 +0800

    modified:   config/database.js 	modified:   package-lock.json 	modified:   package.json

--- back_end/config/database.js
+++ back_end/config/database.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("usertab", "root", "12345678", {
+const sequelize = new Sequelize("usertab", "myuser", "mypassword", {
   host: "127.0.0.1",
   dialect: "mysql",
 });
